4892c1fdd7cc3680039375ca7b5c356b4f2debbc
sungjunpark <spark1498@gmail.com>
2023-09-06 18:28:44 +0900

    23.09.06 code_factory 동영상 플레이어 앱 실습

---
+++ android/app/src/main/kotlin/com/example/codefactory_vid_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codefactory_vid_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:codefactory_vid_player/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
